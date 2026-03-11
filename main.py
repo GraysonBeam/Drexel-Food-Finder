f16e2b0998dbd19f7cf86cd8a806f40fc5e2588b
GraysonB <replaygaming34@gmail.com>
2026-03-11 12:20:12 -0400

    Update main for testing

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from src.scraper import Scraper
+from src.analyzer import Analyzer
+from src.formatter import Formatter
+from src.scraper import DragonLinkScraper
 
-scraper = Scraper()
-links = scraper.scrape()
-print(links)
+scraper = DragonLinkScraper()
+analyzer = Analyzer()
+formatter = Formatter()
+
+links, times, locations = scraper.fetch_valid_events()
+
+for i in range(len(links)):
+    description = scraper.get_description(links[i])
+    if description is not f"Could not find description for {links[i]}":
+        isFood = analyzer.is_free_food(description)
+        if isFood == "TRUE":
+            time = times[i]
+            location = locations[i]
+            formatted_message = formatter.format_message(description, time, location)
+            print(formatted_message, "\n")
